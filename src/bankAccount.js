0407196166480ed9e6e02931b954936b72746622
Samuel Raducan <samuel.thembp@gmail.com>
2022-04-25 17:05:34 +0100

    Add  getBalance method to return the current amount

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,4 +1,12 @@
 class BankAccount {
+	constructor() {
+		this.balance = 0;
+	}
+
+	getBalance() {
+		return this.balance;
+	}
+
 	depositAmount(amount) {
 		return amount;
 	}
--- src/tests/bankAccount.test.js
+++ src/tests/bankAccount.test.js
@@ -3,8 +3,14 @@ const BankAccount = require('../bankAccount');
 describe('#BankAccount', () => {
 	const bankAccount = new BankAccount();
 
+  test('starts with a balance of £0', () => {
+    expect(bankAccount.getBalance()).toBe(0);
+  });
+
 	test('can deposit money', () => {
 		const amount = 1000;
 		expect(bankAccount.depositAmount(amount)).toBe(amount);
 	});
+
+
 });
